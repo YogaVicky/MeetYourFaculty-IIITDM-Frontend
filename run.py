46a831b463d7011c6465631db8d0e0ce07eb6bf3
YogaVicky <yogasrivarshan@gmail.com>
2020-03-20 18:25:43 +0530

    update

--- run.py
+++ run.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify, flash, redirect, session, abort
+from werkzeug import secure_filename
+from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms.fields import DateField
+import os
+import pandas as pd
+import math
+import shutil
+import sys
+from datetime import timedelta
+from cryptography.fernet import Fernet
+from pymysql import connect
+from datetime import datetime
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-   return 'hello world'
+def home():
+   return 'Hello world'
 
 if __name__ == '__main__':
    app.run()
