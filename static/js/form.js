0ce6ec53e75280357e0480d666b9d1f6d341ba8b
imjohnzakkam <johnzakkam2592@gmail.com>
2020-05-02 18:52:41 +0530

    upd

---
+++ static/js/form.js
@@ -0,0 +1,11 @@
+function submit() {
+    alert("The form was submitted");
+  }
+
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
